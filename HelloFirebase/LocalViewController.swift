1393db8637c0f5fd7c974b15f1181db206ac58c9
Aluno R17 <alunor17@M72433.local>
2019-06-11 20:34:51 -0300

    Segundo dia de prova

---
+++ HelloFirebase/LocalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocalViewController.swift
+//  HelloFirebase
+//
+//  Created by Aluno R17 on 07/06/19.
+//  Copyright © 2019 Daniel Valente. All rights reserved.
+//
+
+import UIKit
+
+class LocalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
